b369965570b68fa607445f2f4e05f4bca6ea5e52
Muhammad Firdausi <firdaus251@gmail.com>
2026-06-23 13:51:55 +0300

    edit slurm

--- submit_pinn.sh
+++ submit_pinn.sh
@@ -12,16 +12,14 @@
 #SBATCH --chdir=/home/g202210120/projects/ACOPF_Benchmark
 
 # 1. Load any necessary modules (Uncomment and adjust if your HPC requires it)
-# module load anaconda3/2022.05
-
+module load cuda/12.9.lua
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 # 2. Activate your specific Python/PyTorch environment
-# source activate acopf_env 
-# OR
-# conda activate acopf_env
+source activate pinn_acopf
 
 # 3. Print out hardware info to the log for verification
 echo "Job started on node: $(hostname)"
 echo "Using GPU: $CUDA_VISIBLE_DEVICES"
 
 # 4. Execute the high-speed CUDA training script
-python train_baseline.py
+python ACOPF_pinn_baseline.py
