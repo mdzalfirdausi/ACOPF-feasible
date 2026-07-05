75a51c3d850b071f1b752c548ca7b6541f3a8690
Muhammad Firdausi <firdaus251@gmail.com>
2026-07-05 18:05:16 +0300

    a

--- ACOPF_pinn_baseline.py
+++ ACOPF_pinn_baseline.py
@@ -242,7 +242,7 @@ if __name__ == "__main__":
         "thermal": 1.0,
         "ang": 1.0,
         "v": 1.0,
-        "obj": 1.0
+        "obj": 0.1
     }
 
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
