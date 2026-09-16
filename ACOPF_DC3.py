54fa79243a983ba15f6336d35fd9352b7a03014e
Muhammad Firdausi <firdaus251@gmail.com>
2026-09-16 09:23:01 +0300

    acopf_dc3

--- ACOPF_DC3.py
+++ ACOPF_DC3.py
@@ -345,7 +345,7 @@ if __name__ == "__main__":
             # 2. Checkpointing Logic: If this is the lowest validation loss we've seen, save it!
             if val_loss < best_val_loss:
                 best_val_loss = val_loss
-                state_dict = model._orig_mod.state_dict() if hasattr(model, '_orig_mod') else model.state_dict()
+                state_dict = model_dc3._orig_mod.state_dict() if hasattr(model, '_orig_mod') else model_dc3.state_dict()
                 torch.save(state_dict, model_save_path)
                 saved_flag = " [*SAVED BEST*]"
             else:
